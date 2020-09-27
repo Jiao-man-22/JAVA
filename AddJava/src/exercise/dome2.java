8cf30eed001ee7ea68b28dd8be013caebfe92352
Jiao-man-22 <2935996123@qq.com>
2020-09-27 16:15:36 +0800

    add

--- AddJava/src/exercise/dome2.java
+++ AddJava/src/exercise/dome2.java
@@ -34,10 +34,20 @@ public class dome2 {
 		System.out.println("=========程序结束==================");
 		
 	}
+	public static void printRectangle() {
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<i;j++) {
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		getWays();
+		//getWays();
+		printRectangle();
 	}
 
 }
