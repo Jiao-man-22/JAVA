cc6a72f751adc47751b715405d3ba968d2662eb8
Jiao-man-22 <2935996123@qq.com>
2020-10-10 16:27:53 +0800

    io

--- AddJava/src/exercise/dome6.java
+++ AddJava/src/exercise/dome6.java
@@ -15,6 +15,8 @@ public class dome6 {
 		for(String s:map.keySet()) {
 			System.out.println(s+":"+map.get(s));
 		}
+		
+		//放回set视图 一个map对象
 		for(Map.Entry<String, String> entry: map.entrySet()) {
 			System.out.println(entry.getKey()+":"+entry.getValue());
 		}
